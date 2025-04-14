09acabe70e5a909163e728d4a97031f27f05d1cc
Pedro Nora <65372020+pedronora@users.noreply.github.com>
2025-04-14 19:32:51 +0000

    Correção para fechar todas as conexões.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -16,7 +16,6 @@ export default async function migrations(request, response) {
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
 
@@ -26,13 +25,13 @@ export default async function migrations(request, response) {
       dryRun: false,
     });
 
-    await dbClient.end();
-
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
     return response.status(200).json(migratedMigrations);
   }
+
+  await dbClient.end();
 
   response.status(405).end();
 }
